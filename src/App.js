39633bcfeb3cad92db8d683667b56c32e872b135
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-12 09:35:44 -0300

    Creando el Formulario

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './bootstrap.min.css';
 import Header from './components/Header';
+import NuevaCita from './components/NuevaCita';
 
 class App extends Component {
 	state = {};
@@ -8,6 +9,11 @@ class App extends Component {
 		return (
 			<div class="container">
 				<Header titulo="Administrador Pacientes Veterinaria" />
+				<div className="row">
+					<div className="col-md-10 mx-auto">
+						<NuevaCita />
+					</div>
+				</div>
 			</div>
 		);
 	}
